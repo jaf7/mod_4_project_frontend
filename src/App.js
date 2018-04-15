f1c8c283fb20c13731af07063689fe7a47543f5a
jaf7 <janthonyfields@gmail.com>
2018-04-15 10:20:06 -0400

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
   Route
